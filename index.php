77a891e55db8ccdfd35d0775ca166a370bc93824
carlosAlx <53825590+carlosAlx@users.noreply.github.com>
2022-03-16 11:36:08 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/assets//css/estilo.css">
+        <link rel="stylesheet" href="/assets//css/reset.css">
+        <link rel="stylesheet" href="/assets//css/home.css">
+        <script src="https://kit.fontawesome.com/c190036b73.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <?php require('template.html'); ?>
+        <section class="section-banner" id="banner">
+            <div class="banner">
+                <img class="img-banner" src="/assests/img/unnamed.png">
+            </div>
+            <div>
+            </div>
+        </section>
+
+        <section id=services>
+            <div class="container border-green">
+                <div class="col-3">
+                   <div>
+                       <h3>Internet Fibra,Planos Oi e mais serviços pra você</h3>
+                   </div>
+
+                </div>
+                <div class="col-9 ">
+                    <div class="d-flex h-green">
+                        <div class="col-5">
+                            <h2>Internet OI fibra</h2>
+                            <p>Contrate planos de internet residencial com até 1 Giga de velocidade e tenha a melhor experiência pra trabalhar ou estudar online,
+                                jogar, assistir filmes e séries com a internet fibra ótica da Oi.</p>
+                        </div>
+                        <div class="col-6">
+                            <img src="">
+
+                        </div>
+                    </div>
+                    <div class="d-flex h-green">
+                        <div class="col-5">
+                            <h2>Internet OI fibra</h2>
+                            <p>Contrate planos de internet residencial com até 1 Giga de velocidade e tenha a melhor experiência pra trabalhar ou estudar online,
+                                jogar, assistir filmes e séries com a internet fibra ótica da Oi.</p>
+                        </div>
+                        <div class="col-6">
+                            <img src="">
+
+                        </div>
+                    </div>
+                    <div class="d-flex h-green">
+                        <div class="col-5">
+                            <h2>Internet OI fibra</h2>
+                            <p>Contrate planos de internet residencial com até 1 Giga de velocidade e tenha a melhor experiência pra trabalhar ou estudar online,
+                                jogar, assistir filmes e séries com a internet fibra ótica da Oi.</p>
+                        </div>
+                        <div class="col-6">
+                            <img src="">
+
+                        </div>
+                    </div>   
+                    <div class="d-flex h-green">
+                        <div class="col-5">
+                            <h2>Internet OI fibra</h2>
+                            <p>Contrate planos de internet residencial com até 1 Giga de velocidade e tenha a melhor experiência pra trabalhar ou estudar online,
+                                jogar, assistir filmes e séries com a internet fibra ótica da Oi.</p>
+                        </div>
+                        <div class="col-6">
+                            <img src="">
+
+                        </div>
+                    </div>     
+                </div> 
+        </section>
+
+        <section>
+            <div class="container">              
+                    <div class="col-3">
+                        <h1>Hoje tem as melhores ofertas pra você</h1>
+                    </div>
+                    <div class="col-3">
+                        <h2>Oi Fibra</h2>
+                        <p>Internet Fibra Ótica com alta velocidade e estabilidade pra você navegar à vontade, trabalhar ou estudar online,
+                             jogar, assistir filmes e séries.</p>
+                    </div>
+                    <div class="col-3">
+                        <h2>Oi Fibra</h2>
+                        <p>Internet Fibra Ótica com alta velocidade e estabilidade pra você navegar à vontade, trabalhar ou estudar online,
+                             jogar, assistir filmes e séries.</p>
+                    </div>
+                    <div class="col-3">
+                        <h2>Oi Fibra</h2>
+                        <p>Internet Fibra Ótica com alta velocidade e estabilidade pra você navegar à vontade, trabalhar ou estudar online,
+                             jogar, assistir filmes e séries.</p>
+                    </div>             
+            </div>
+        </section>
+
+        <?php require('footer.html') ?>
+    </body>
+</html>
---
+++ readme.txt
@@ -0,0 +1,3 @@
+reset.css contem css do elemtos como div/ body
+
+estilo.css contem css de elementos de text/botoom
